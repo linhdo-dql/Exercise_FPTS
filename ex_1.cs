2a9324597636d5fd5be32eb0ae6a40129f4a5e16
linhdo <linhlee88999@gmai.com>
2021-11-01 11:16:14 +0700

    Exercise 1

---
+++ ex_1.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Exercise_1
+{
+    class Exercise1
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("Hello World");
+        }
+    }
+}
